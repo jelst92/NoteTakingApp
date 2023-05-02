d02a99817eb05df83ff2370f0d4df70cc4d2f827
jelst92 <joseph.parker92@outlook.com>
2023-05-02 14:37:56 +0100

    Added button to save. Then another to load notes. Needs further work to show existing notes within app. Perhaps add a new screen.

--- NoteTakingApp/Form1.Designer.cs
+++ NoteTakingApp/Form1.Designer.cs
@@ -28,18 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(527, 459);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 0;
+            button1.Text = "Save Note";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(49, 150);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(710, 287);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 32);
+            label1.TabIndex = 2;
+            label1.Text = "Note Taking App";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(175, 459);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 3;
+            button2.Text = "Load Notes";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 531);
+            ClientSize = new Size(814, 528);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button2;
     }
 }
--- NoteTakingApp/Form1.cs
+++ NoteTakingApp/Form1.cs
@@ -6,5 +6,41 @@ namespace NoteTakingApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the contents of the text box
+            string contents = textBox1.Text;
+
+            // Write the contents to a file
+            System.IO.File.WriteAllText(@"C:\Users\Josep\source\repos\NoteTakingApp\Notes.txt", contents);
+
+            // Show a message box to confirm the save
+            MessageBox.Show("Notes saved successfully.");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                // Show the Open File dialog box
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                openFileDialog1.Title = "Open Note";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // Read the contents of the selected file and display them in the text box
+                    string fileName = openFileDialog1.FileName;
+                    string contents = File.ReadAllText(fileName);
+                    textBox1.Text = contents;
+                }
+            }
+
+            private void editButton_Click(object sender, EventArgs e)
+            {
+                // Enable editing of the text box
+                textBox1.ReadOnly = false;
+            }
+
+        }
     }
-}
